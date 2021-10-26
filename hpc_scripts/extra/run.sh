ede51f796fab8371876d3edd4ffae29dda5c6ea2
Aïvodji <a.u.matchi@gmail.com>
2021-10-26 10:55:32 -0400

    experiments on detactability

--- hpc_scripts/extra/run.sh
+++ hpc_scripts/extra/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=02:00:00
+#SBATCH --time=00:30:00
 #SBATCH --ntasks=301
 #SBATCH --mem-per-cpu=4G
 
@@ -9,18 +9,8 @@ cd ../../core
 
 
 
-## Logistic regression
-srun python LaundryML_Fairlearn.py --dataset=1 --rseed=6 --metric=1 --explainer=lm  --model_class=XgBoost --transfer
-srun python LaundryML_Fairlearn.py --dataset=4 --rseed=2 --metric=3 --explainer=lm  --model_class=XgBoost --transfer
-
-# Decision tree
-srun python LaundryML_Fairlearn.py --dataset=1 --rseed=5 --metric=5 --explainer=dt  --model_class=RF --transfer
-srun python LaundryML_Fairlearn.py --dataset=1 --rseed=7 --metric=4 --explainer=dt  --model_class=AdaBoost --transfer
-srun python LaundryML_Fairlearn.py --dataset=3 --rseed=9 --metric=1 --explainer=dt  --model_class=DNN --transfer
-srun python LaundryML_Fairlearn.py --dataset=4 --rseed=2 --metric=5 --explainer=dt  --model_class=AdaBoost --transfer
-srun python LaundryML_Fairlearn.py --dataset=4 --rseed=4 --metric=1 --explainer=dt  --model_class=RF --transfer
-
-
+## Rule List
+srun python LaundryML.py --dataset=4 --rseed=8 --metric=1  --model_class=XgBoost --transfer
 
 
 

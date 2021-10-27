dc809f1204419ce69a2dc09f2b503cb41e34cb9a
Aïvodji <a.u.matchi@gmail.com>
2021-10-27 18:53:28 -0400

    experiments on detactability

--- models/main.sh
+++ models/main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#datasets=(adult_income compas default_credit marketing)
-datasets=(adult_income)
+datasets=(adult_income compas default_credit marketing)
 models=(AdaBoost DNN RF XgBoost)
 rseed=(0 1 2 3 4 5 6 7 8 9)
 
